6a27b13cb7398757cd17b90af827f69dbb1ce9d8
Heliandro Lavor <heliandro.l@gmail.com>
2024-08-18 19:38:21 -0300

    refactor: transforma a classe KafkaMessage em record para enxugar o codigo

--- kafka-demo/src/main/java/com/example/kafka_demo/controllers/KafkaController.java
+++ kafka-demo/src/main/java/com/example/kafka_demo/controllers/KafkaController.java
@@ -18,25 +18,4 @@ public class KafkaController {
     }
 }
 
-class KafkaMessage {
-    private String key;
-    private String value;
-
-    // Getters and setters
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-}
+record KafkaMessage(String key, String value) {}
